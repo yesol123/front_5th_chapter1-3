1ba999136d984dcf832b36bb1450df80d683e4b4
yesolkim <yes123@lukus.kr>
2025-04-09 12:38:54 +0900

    ✨ feat: equalities 만들기

--- src/@lib/equalities/deepEquals.ts
+++ src/@lib/equalities/deepEquals.ts
@@ -1,3 +1,39 @@
+// deepEquals 함수는 두 값의 깊은 비교를 수행합니다.
+
 export function deepEquals<T>(objA: T, objB: T): boolean {
-  return objA === objB;
+  // 1. 기본 타입
+  if (objA === objB) return true;
+  //null인 경우이거나 타입 다르면 비교 의미 없음음
+  if (objA === null || objB === null || typeof objA !== typeof objB) {
+    return false;
+  }
+
+  // 2. 둘 다 객체인 경우:
+  if (typeof objA === "object" && typeof objB === "object") {
+    //    - 객체의 키 개수가 다른 경우 처리
+    const keysA = Object.keys(objA);
+    const keysB = Object.keys(objB);
+    if (keysA.length !== keysB.length) return false;
+
+    for (const key of keysA) {
+      //objB 객체가 해당 key를 직접 가지고 있는지 확인한다.
+      if (!Object.prototype.hasOwnProperty.call(objB, key)) return false;
+      // objA와 objB의 해당 키 값을 재귀적으로 깊은 비교하라.
+      if (!deepEquals((objA as any)[key], (objB as any)[key])) return false;
+    }
+
+    //  - 배열인지 확인 (deepEquals는 내용을 하나하나 비교해야 true로 판단 가능)
+    if (Array.isArray(objA) && Array.isArray(objB)) {
+      if (objA.length !== objB.length) return false;
+
+      //정확히 비교
+      for (let i = 0; i < objA.length; i++) {
+        // - 재귀적으로 각 속성에 대해 deepEquals 호출
+        if (!deepEquals(objA[i], objB[i])) return false;
+      }
+    }
+    return true;
+  }
+
+  return false;
 }
--- src/@lib/equalities/shallowEquals.ts
+++ src/@lib/equalities/shallowEquals.ts
@@ -1,3 +1,38 @@
+// shallowEquals 함수는 두 값의 얕은 비교를 수행합니다.
+
 export function shallowEquals<T>(objA: T, objB: T): boolean {
-  return objA === objB;
+  // 1. 두 값이 정확히 같은지 확인 (참조가 같은 경우)
+  if (objA === objB) {
+    return true;
+  }
+  // 2. 둘 중 하나라도 객체가 아닌 경우 처리 (객체가 아니거나 null 인경우)
+  if (
+    typeof objA !== "object" ||
+    objA === null ||
+    typeof objB !== "object" ||
+    objB === null
+  ) {
+    return false;
+  }
+  // 3. 객체의 키 개수가 다른 경우 처리
+  const keysA = Object.keys(objA);
+  const keysB = Object.keys(objB);
+  if (keysA.length !== keysB.length) {
+    return false;
+  }
+
+  // 4. 모든 키에 대해 얕은 비교 수행
+
+  for (const key of keysA) {
+    //keysA는 Object.keys(objA)로 얻은 문자열 배열
+    // hasOwnProperty(key)해당 키(속성)가 객체에 직접 존재하는지를 확인
+    if (
+      !objB.hasOwnProperty(key) ||
+      (objA as any)[key] !== (objB as any)[key]
+    ) {
+      return false;
+    }
+  }
+
+  return true;
 }
